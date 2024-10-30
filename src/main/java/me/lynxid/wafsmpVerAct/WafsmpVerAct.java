e663378eec28e033d5afd7b6788a58087d980b60
mothissoup <thetruesilo@gmail.com>
2024-10-29 22:35:22 -0400

    Ver 1.1-SNAPSHOT This version adds the way cool brand new never seen feature of logging when someone joins the server!! (can you guys tell im following a guide yet?)

--- src/main/java/me/lynxid/wafsmpVerAct/WafsmpVerAct.java
+++ src/main/java/me/lynxid/wafsmpVerAct/WafsmpVerAct.java
@@ -1,14 +1,27 @@
 package me.lynxid.wafsmpVerAct;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class WafsmpVerAct extends JavaPlugin {
+public final class WafsmpVerAct extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
 
         System.out.println("Wafsmp VerAct has started!");
+
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+
+        System.out.println("A Player Has joined");
+
     }
 
     @Override
