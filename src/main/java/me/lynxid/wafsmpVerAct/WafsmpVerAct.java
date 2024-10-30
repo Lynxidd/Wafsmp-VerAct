292224a9f8103cd1b60119738490d61280a74b8a
mothissoup <thetruesilo@gmail.com>
2024-10-29 22:07:00 -0400

    Begin Creation

---
+++ src/main/java/me/lynxid/wafsmpVerAct/WafsmpVerAct.java
@@ -0,0 +1,20 @@
+package me.lynxid.wafsmpVerAct;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WafsmpVerAct extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        System.out.println("Wafsmp VerAct has started!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+        System.out.println("Wafsmp VerAct has stopped.");
+    }
+}
