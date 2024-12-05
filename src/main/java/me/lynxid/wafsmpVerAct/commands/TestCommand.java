6e2e6da11709fc29d6eff747fd78a8b4e2b87859
mothissoup <thetruesilo@gmail.com>
2024-12-05 16:13:47 -0500

    1.15-SNAPSHOT Changed the version to 1.15, and adjusted Test

--- src/main/java/me/lynxid/wafsmpVerAct/commands/TestCommand.java
+++ src/main/java/me/lynxid/wafsmpVerAct/commands/TestCommand.java
@@ -6,6 +6,7 @@ import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -14,6 +15,7 @@ import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitScheduler;
 
 import java.io.File;
 import java.io.IOException;
@@ -60,66 +62,59 @@ public class TestCommand implements CommandExecutor {
                     i.printStackTrace();
                 }
 
+                BukkitScheduler scheduler = Bukkit.getScheduler();
+                scheduler.runTaskTimer(plugin, task -> {
 
-                getServer().getScheduler().runTaskTimer(plugin, new Runnable() {
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage("\uE000");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(" ");
+                    target.sendMessage(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Rules of the WSMP");
+                    target.sendMessage(" ");
+                    target.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Rule 1. " + ChatColor.WHITE + "Don’t do anything intended to make someone unhappy. Don’t make fun of people, and no racism, homophobia, transphobia, etc. While swearing is allowed (in moderation), slurs are NEVER allowed.");
+                    target.sendMessage(" ");
+                    target.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Rule 2. " + ChatColor.WHITE + "Minecraft is a game that lots of younger kids play. No NSFW, In chat or built on the server, Don’t talk about anything unpleasant or controversial. Everyone is here to play a game and have fun.");
+                    target.sendMessage(" ");
+                    TextComponent msg = new TextComponent("[Next Page]");
+                    msg.setColor(ChatColor.DARK_GREEN.asBungee());
+                    msg.setBold(true);
 
-                    @Override
-                    public void run() {
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage("\uE000");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(" ");
-                        target.sendMessage(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Rules of the WSMP");
-                        target.sendMessage(" ");
-                        target.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Rule 1. " + ChatColor.WHITE + "Don’t do anything intended to make someone unhappy. Don’t make fun of people, and no racism, homophobia, transphobia, etc. While swearing is allowed (in moderation), slurs are NEVER allowed.");
-                        target.sendMessage(" ");
-                        target.sendMessage(ChatColor.GOLD + "" + ChatColor.BOLD + "Rule 2. " + ChatColor.WHITE + "Minecraft is a game that lots of younger kids play. No NSFW, In chat or built on the server, Don’t talk about anything unpleasant or controversial. Everyone is here to play a game and have fun.");
-                        target.sendMessage(" ");
-                        TextComponent msg = new TextComponent("[Next Page]");
-                        msg.setColor(ChatColor.DARK_GREEN.asBungee());
-                        msg.setBold(true);
-
-                        msg.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules p2"));
-                        msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to go to the next page")));
-                        target.spigot().sendMessage(msg);
-
-                    }
-
-                }, 0L,  20L); // 20 ticks = 1 second.
+                    msg.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules p2"));
+                    msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to go to the next page")));
+                    target.spigot().sendMessage(msg);
 
 
+                    task.cancel();
 
+                }, 0L, 20L );
 
             }
             return true;
-
         }
-
-
         return true;
     }
 }
