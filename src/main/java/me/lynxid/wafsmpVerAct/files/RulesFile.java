7c3a452c638afef12c447ee0a5361da6734f6781
WafflesAreBetter <wafflesarebetter@wafcorp.org>
2025-01-11 15:18:18 -0700

    1.0.1-PRE-RELEASE Rules UI update!
    Changed the default rules in RulesFile to sound good and make sense

--- src/main/java/me/lynxid/wafsmpVerAct/files/RulesFile.java
+++ src/main/java/me/lynxid/wafsmpVerAct/files/RulesFile.java
@@ -52,10 +52,11 @@ public class RulesFile {
         RulesFile.get().set("Setup run", true);
         RulesFile.get().set("logo", "\uE000");
         RulesFile.get().set("rules.1", "Don’t do anything intended to make someone unhappy. Don’t make fun of people, and no racism, homophobia, transphobia, etc will be tolerated. While swearing is allowed (in moderation), slurs are NEVER allowed.");
-        RulesFile.get().set("rules.2", "Minecraft is a game that lots of younger kids play. No NSFW, In chat or built on the server, Don’t talk about anything unpleasant or controversial. Everyone is here to play a game and have fun.");
-        RulesFile.get().set("rules.3","All of the rules listed above apply everywhere in the server and are enforced by admins, but every player-made country has their own rules that their leaders enforce. Make sure that while in an in-game country, you follow their laws and international laws.");
-        RulesFile.get().set("rules.4","No cheating, hacking, or exploiting! Automatic farms are allowed but duping is not, the only things you can duplicate are sand/gravel/concrete and tnt, as these are non-renewable resources. Again any form of finding ores that wasn’t intended by Mojang is prohibited");
-        RulesFile.get().set("rules.5","This includes spam in chat or purposefully building at someone else’s base just to annoy them, or anything else meant to annoy someone.");
-        RulesFile.get().set("rules.6","Don’t share anyone else’s personal information, no discussion of illegal activity. the usage or discussion of cracked/free Minecraft accounts will get you banned!");
+        RulesFile.get().set("rules.2", "Minecraft is a game that lots of younger kids play. No NSFW, either in chat or built on the server, and don’t talk about anything unpleasant or controversial. Everyone is here to play a game and have fun.");
+        RulesFile.get().set("rules.3", "No cheating, hacking, or exploiting! Automatic farms are allowed but item duping is not, the only things you can duplicate are sand/gravel/concrete and tnt, as these are non-renewable resources. Don't use Any form of finding ores or structures that wasn’t intended by Mojang.");
+        RulesFile.get().set("rules.4", "Be a good person - don’t share anyone else’s personal information, and don't discuss illegal activity, including cracked or free Minecraft accounts.");
+        RulesFile.get().set("rules.5", "Don't be a pest! This includes spam in chat or purposefully building at someone else’s base just to annoy them, or anything else meant to bother someone.");
+        RulesFile.get().set("rules.6", "All of the rules listed above apply everywhere in the server and are enforced by admins, but every player-made country has their own rules that their leaders enforce, and international laws (including no griefing / stealing) apply everywhere else. Make sure to follow the law.");
+
     }
 }
