3e371a5015ce9a916221e6bbee0ab8d4c0fb2fe9
WafflesAreBetter <wafflesarebetter@wafcorp.org>
2025-01-11 15:01:30 -0700

    1.0.1-PRE-RELEASE Rules UI update!
    Finished overhaul of the RulesCommand class so that it should now work for any number of rules and loop the messages every 4 ticks

--- src/main/java/me/lynxid/wafsmpVerAct/commands/RulesCommand.java
+++ src/main/java/me/lynxid/wafsmpVerAct/commands/RulesCommand.java
@@ -7,6 +7,7 @@ import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.chat.hover.content.Text;
 import org.bukkit.ChatColor;
+import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -14,6 +15,8 @@ import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 
@@ -40,29 +43,24 @@ public class RulesCommand implements CommandExecutor {
         FileConfiguration playerData = YamlConfiguration.loadConfiguration(file);
 
         String logo = RulesFile.get().getString("logo");
-        String rule1 = RulesFile.get().getString("rule1");
-        String rule2 = RulesFile.get().getString("rule2");
-        String rule3 = RulesFile.get().getString("rule3");
-        String rule4 = RulesFile.get().getString("rule4");
-        String rule5 = RulesFile.get().getString("rule5");
-        String rule6 = RulesFile.get().getString("rule6");
         List<String> rulesList = RulesFile.get().getStringList("rules");
 
+        if (rulesList.isEmpty()) return true;
+        int totalPages = ((rulesList.size() % 2 == 0) ? rulesList.size() : rulesList.size() + 1) / 2;
 
-        if (strings.length < 1 || strings[0].equalsIgnoreCase("p1")) {
-            // TODO: Store that the player is on page 1
-            page1(p, logo, rule1, rule2);
+        PersistentDataContainer pdc = p.getPersistentDataContainer();
+        NamespacedKey storedRulesPage = new NamespacedKey((Plugin) this, "currentRulesPage");
 
-        } else if (strings[0].equalsIgnoreCase("p2")) {
-            // TODO: Store that the player is on page 2
-            page2(p, logo, rule3, rule4);
+        if (strings.length < 1) {
+            pdc.set(storedRulesPage, PersistentDataType.INTEGER, 1);
 
-        } else if (strings[0].equalsIgnoreCase("p3")) {
-            // TODO: Store that the player is on page 3
-            page3(p, logo, rule5, rule6);
+            String firstRule = rulesList.getFirst();
+            String secondRule = (rulesList.size() > 1) ? rulesList.get(1) : "";
+            sendRulesPage(p, logo, firstRule, secondRule, 1, totalPages);
+        }
+        else if (strings[0].equalsIgnoreCase("accept")) {
+            pdc.set(storedRulesPage, PersistentDataType.INTEGER, -2);
 
-        } else if (strings[0].equalsIgnoreCase("accept")) {
-            // TODO: Store that the player is on page -1
             p.sendMessage("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
             sender.getServer().broadcastMessage(ChatColor.GOLD + "" + ChatColor.BOLD + p.getDisplayName() + " has accepted the rules! Welcome to the server!");
             effectsTake();
@@ -77,6 +75,24 @@ public class RulesCommand implements CommandExecutor {
                 getLogger().severe(i.toString());
             }
         }
+        else if (strings[0].startsWith("p")) {
+            int pageNum;
+            try {
+               pageNum = Integer.parseInt(strings[0].substring(1));
+            } catch (NumberFormatException e) {
+                p.sendMessage(ChatColor.RED + "Please enter a valid page number! Ex: p1");
+                return true;
+            }
+
+            if (pageNum < 1) pageNum = 1;
+            else if (pageNum > totalPages) pageNum = totalPages;
+
+            pdc.set(storedRulesPage, PersistentDataType.INTEGER, pageNum);
+
+            String firstRule = rulesList.get(((pageNum - 1) * 2) + 1);
+            String secondRule = (((pageNum - 1) * 2) + 2 <= rulesList.size()) ? rulesList.get(((pageNum - 1) * 2) + 2) : "";
+            sendRulesPage(p, logo, firstRule, secondRule, pageNum, totalPages);
+        }
 
         return true;
     }
@@ -87,81 +103,49 @@ public class RulesCommand implements CommandExecutor {
         p.sendMessage(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Rules of the WSMP");
     }
 
-    public void page1(Player p, String logo, String rule1, String rule2) {
+    public void sendRulesPage(Player p, String logo, String firstRule, String secondRule, int page, int totalPages) {
         sendRulesHeader(p, logo);
-        p.sendMessage(ChatColor.GOLD + "\n" + ChatColor.BOLD + "Rule 1. " + ChatColor.WHITE + rule1);
-        p.sendMessage(ChatColor.GOLD + "\n" + ChatColor.BOLD + "Rule 2. " + ChatColor.WHITE + rule2 + "\n");
+        p.sendMessage(ChatColor.GOLD + "\n" + ChatColor.BOLD + "Rule " + (((page - 1) * 2) + 1) + ". " + ChatColor.WHITE + firstRule);
+        if (!secondRule.isEmpty()) p.sendMessage(ChatColor.GOLD + "\n" + ChatColor.BOLD + "Rule " + (((page - 1) * 2) + 2) + ". " + ChatColor.WHITE + secondRule + "\n");
 
-        TextComponent msg = new TextComponent("[Next Page]");
-        msg.setColor(ChatColor.DARK_GREEN.asBungee());
-        msg.setBold(true);
-        msg.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules p2"));
-        msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to go to the next page")));
+        TextComponent previousPage = new TextComponent("");
+        TextComponent nextPage = new TextComponent("");
+        TextComponent accept = new TextComponent("");
 
-        p.spigot().sendMessage(msg);
-
-        p.sendMessage();
-
-        if (true) { // TODO: Make sure the player is still on page 1
-            getServer().getScheduler().scheduleSyncDelayedTask((Plugin) this, () -> {
-                page1(p, logo, rule1, rule2);
-
-            }, 4L);
+        if (page != 1) {
+            previousPage = new TextComponent("[Previous Page] ");
+            previousPage.setColor(ChatColor.DARK_GREEN.asBungee());
+            previousPage.setBold(true);
+            previousPage.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules p" + (page - 1)));
+            previousPage.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to go to the previous page")));
         }
-    }
-
-
-
-    public void page2(Player p, String logo, String rule3, String rule4) {
-        sendRulesHeader(p, logo);
-        p.sendMessage(ChatColor.GOLD + "\n" + ChatColor.BOLD + "Rule 3. " + ChatColor.WHITE + rule3);
-        p.sendMessage(ChatColor.GOLD + "\n" + ChatColor.BOLD + "Rule 4. " + ChatColor.WHITE + rule4 + "\n");
-
-        TextComponent msg2 = new TextComponent("[Previous Page] ");
-        msg2.setColor(ChatColor.DARK_GREEN.asBungee());
-        msg2.setBold(true);
-        msg2.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules"));
-        msg2.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to go to the previous page")));
-
-        TextComponent msg = new TextComponent("[Next Page]");
-        msg.setColor(ChatColor.DARK_GREEN.asBungee());
-        msg.setBold(true);
-        msg.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules p3"));
-        msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to go to the next page")));
-
-        p.spigot().sendMessage(msg2, msg);
-
-        if (true) { // TODO: Make sure the player is still on page 2
-            getServer().getScheduler().scheduleSyncDelayedTask((Plugin) this, () -> {
-                page2(p, logo, rule3, rule4);
-
-            }, 4L);
+        if (page != totalPages) {
+            nextPage = new TextComponent("[Next Page]");
+            nextPage.setColor(ChatColor.DARK_GREEN.asBungee());
+            nextPage.setBold(true);
+            nextPage.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules p" + (page + 1)));
+            nextPage.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to go to the next page")));
         }
-    }
+        else {
+            accept = new TextComponent("[Accept rules]");
+            accept.setColor(ChatColor.DARK_GREEN.asBungee());
+            accept.setBold(true);
+            accept.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules accept"));
+            accept.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to accept the rules")));
+        }
 
-    public void page3(Player p, String logo, String rule5, String rule6) {
-        sendRulesHeader(p, logo);
-        p.sendMessage(ChatColor.GOLD + "\n" + ChatColor.BOLD + "Rule 5. " + ChatColor.WHITE + rule5);
-        p.sendMessage(ChatColor.GOLD + "\n" + ChatColor.BOLD + "Rule 6. " + ChatColor.WHITE + rule6 + "\n");
+        p.spigot().sendMessage(previousPage, nextPage, accept);
 
-        TextComponent msg2 = new TextComponent("[Previous Page] ");
-        msg2.setColor(ChatColor.DARK_GREEN.asBungee());
-        msg2.setBold(true);
-        msg2.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules p2"));
-        msg2.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to go to the previous page")));
+        Integer currentRulesPage = 0;
+        PersistentDataContainer pdc = p.getPersistentDataContainer();
+        NamespacedKey storedRulesPage = new NamespacedKey((Plugin) this, "currentRulesPage");
+        if (pdc.has(storedRulesPage, PersistentDataType.INTEGER)) {
+            currentRulesPage = pdc.get(storedRulesPage, PersistentDataType.INTEGER);
+        }
 
-        TextComponent msg = new TextComponent("[Accept rules]");
-        msg.setColor(ChatColor.DARK_GREEN.asBungee());
-        msg.setBold(true);
-        msg.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/rules accept"));
-        msg.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to accept the rules")));
-
-        p.spigot().sendMessage(msg2, msg);
-
-        if (true) { // TODO: Make sure the player is still on page 3
+        if (currentRulesPage != null && page == currentRulesPage) {
             getServer().getScheduler().scheduleSyncDelayedTask((Plugin) this, () -> {
-                page3(p, logo, rule5, rule6);
-
+                sendRulesPage(p, logo, firstRule, secondRule, page, totalPages);
             }, 4L);
         }
     }
