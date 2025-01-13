c5a1fceb98016bb9b4a2ca0349ee11a9dc347e9b
WafflesAreBetter <wafflesarebetter@wafcorp.org>
2025-01-12 18:22:50 -0700

    1.0.1-PRE-RELEASE
    Changed RulesFile to save the default rules as an arraylist instead of whatever it was before so that it'll work with loading in RulesCommand

--- src/main/java/me/lynxid/wafsmpVerAct/files/RulesFile.java
+++ src/main/java/me/lynxid/wafsmpVerAct/files/RulesFile.java
@@ -7,6 +7,8 @@ import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import static org.bukkit.Bukkit.getLogger;
 
@@ -51,12 +53,15 @@ public class RulesFile {
     public static void setDefault() {
         RulesFile.get().set("Setup run", true);
         RulesFile.get().set("logo", "\uE000");
-        RulesFile.get().set("rules.1", "Don’t do anything intended to make someone unhappy. Don’t make fun of people, and no racism, homophobia, transphobia, etc will be tolerated. While swearing is allowed (in moderation), slurs are NEVER allowed.");
-        RulesFile.get().set("rules.2", "Minecraft is a game that lots of younger kids play. No NSFW, either in chat or built on the server, and don’t talk about anything unpleasant or controversial. Everyone is here to play a game and have fun.");
-        RulesFile.get().set("rules.3", "No cheating, hacking, or exploiting! Automatic farms are allowed but item duping is not, the only things you can duplicate are sand/gravel/concrete and tnt, as these are non-renewable resources. Don't use any form of finding ores or structures that wasn’t intended by Mojang.");
-        RulesFile.get().set("rules.4", "Be a good person - don’t share anyone else’s personal information, and don't discuss illegal activity, including cracked or free Minecraft accounts.");
-        RulesFile.get().set("rules.5", "Don't be a pest! This includes spam in chat or purposefully building at someone else’s base just to annoy them, or anything else meant to bother someone.");
-        RulesFile.get().set("rules.6", "All of the rules listed above apply everywhere in the server and are enforced by admins, but every player-made country has their own rules that their leaders enforce, and international laws (including no griefing / stealing) apply everywhere else. Make sure to follow the law.");
 
+        List<String> rulesList = new ArrayList<>();
+        rulesList.add("Don’t do anything intended to make someone unhappy. Don’t make fun of people, and no racism, homophobia, transphobia, etc will be tolerated. While swearing is allowed (in moderation), slurs are NEVER allowed.");
+        rulesList.add("Minecraft is a game that lots of younger kids play. No NSFW, either in chat or built on the server, and don’t talk about anything unpleasant or controversial. Everyone is here to play a game and have fun.");
+        rulesList.add("No cheating, hacking, or exploiting! Automatic farms are allowed but item duping is not, the only things you can duplicate are sand/gravel/concrete and tnt, as these are non-renewable resources. Don't use any form of finding ores or structures that wasn’t intended by Mojang.");
+        rulesList.add("Be a good person - don’t share anyone else’s personal information, and don't discuss illegal activity, including cracked or free Minecraft accounts.");
+        rulesList.add("Don't be a pest! This includes spam in chat or purposefully building at someone else’s base just to annoy them, or anything else meant to bother someone.");
+        rulesList.add("All of the rules listed above apply everywhere in the server and are enforced by admins, but every player-made country has their own rules that their leaders enforce, and international laws (including no griefing / stealing) apply everywhere else. Make sure to follow the law.");
+
+        RulesFile.get().set("rules", rulesList);
     }
 }
